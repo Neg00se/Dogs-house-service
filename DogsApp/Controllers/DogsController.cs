f1af464a9dcc83027bc95da2e5cd1f4dc3af60bb
Neg00se <evg.matyushko@gmail.com>
2024-10-31 01:19:41 +0200

    Added dog controller

---
+++ DogsApp/Controllers/DogsController.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogic.Interfaces;
+using BusinessLogic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogsApp.Controllers;
+//[Route("api/[controller]")]
+[ApiController]
+public class DogsController : ControllerBase
+{
+    private readonly IDogsService _dogService;
+
+    public DogsController(IDogsService dogService)
+    {
+        _dogService = dogService;
+    }
+
+    [HttpGet("dogs")]
+    public async Task<ActionResult<IEnumerable<DogModel>>> Get([FromQuery] QueryModel queryModel)
+    {
+        var dogs = await _dogService.GetDogsAsync(queryModel);
+        if (dogs == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(dogs);
+    }
+
+
+    [HttpPost("dog")]
+    public async Task<ActionResult> Add(DogModel dog)
+    {
+        try
+        {
+            await _dogService.AddAsync(dog);
+            return Ok();
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+
+            return BadRequest(ex.Message);
+        }
+    }
+}
