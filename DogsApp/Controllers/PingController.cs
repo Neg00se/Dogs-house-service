11617325d9de96559d1144d6395499d26efab5e0
Neg00se <evg.matyushko@gmail.com>
2024-10-31 01:06:47 +0200

    Add ping controller

---
+++ DogsApp/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogsApp.Controllers;
+//[Route("api/[controller]")]
+[ApiController]
+public class PingController : ControllerBase
+{
+
+    [HttpGet("ping")]
+    public ActionResult<string> Ping()
+    {
+        return "Dogshouseservice.Version1.0.1";
+    }
+}
