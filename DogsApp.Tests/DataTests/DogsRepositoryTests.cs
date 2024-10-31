c4d5f25c767347c729cf1e7f5a633fd6e791eecf
Neg00se <evg.matyushko@gmail.com>
2024-10-31 23:25:27 +0200

    Added unit tests for dogs repository

---
+++ DogsApp.Tests/DataTests/DogsRepositoryTests.cs
@@ -0,0 +1,73 @@
+﻿using DataAccess.Entities;
+using DataAccess.Repositories;
+using FluentAssertions;
+
+namespace DogsApp.Tests.DataTests;
+
+public class DogsRepositoryTests
+{
+
+    [Fact]
+    public async void DogsRepository_GetDogsAsync_ReturnsDogs()
+    {
+        //Arrange
+
+        using var dbContext = await UnitTestsHelper.GetDatabaseContext();
+        var dogsRepo = new DogsRepository(dbContext);
+
+        //Act
+
+        var result = await dogsRepo.GetDogsAsync();
+
+        //Assert
+
+        result.Should().BeOfType<List<Dog>>();
+        result.Should().BeEquivalentTo(ExpectedDogs);
+        result.Should().HaveCount(3);
+    }
+
+    [Fact]
+    public async void DogsRepository_GetDogsAsync_WithPageSize1_ReturnsFirstDog()
+    {
+        //Arrange
+
+        using var dbContext = await UnitTestsHelper.GetDatabaseContext();
+        var dogRepo = new DogsRepository(dbContext);
+
+
+        //Act
+
+        var result = await dogRepo.GetDogsAsync(1, 1);
+
+        //Assert
+
+        result.Should().BeOfType<List<Dog>>();
+        result.Should().ContainEquivalentOf(ExpectedDogs.First());
+        result.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async void DogsRepository_AddDogAsync_AddsValueToDb()
+    {
+        //Arrange
+
+        using var dbContext = await UnitTestsHelper.GetDatabaseContext();
+        var dogRepo = new DogsRepository(dbContext);
+
+        Dog dog = new Dog() { Name = "Doggo", Color = "blue", TailLength = 12, Weight = 3 };
+        //Act
+
+        await dogRepo.AddDogAsync(dog);
+
+        //Assert
+
+        dbContext.Dogs.Should().HaveCount(4);
+    }
+
+    private static IEnumerable<Dog> ExpectedDogs =>
+    [
+        new Dog { Name = "Neo", Color = "red&amber", TailLength = 22, Weight = 32 },
+          new Dog { Name = "Jessy", Color = "black&yellow", TailLength = 7, Weight = 14 },
+             new Dog { Name = "Doggy", Color = "red", TailLength = 173, Weight = 33 }
+    ];
+}
---
+++ DogsApp.Tests/UnitTestsHelper.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogsApp.Tests;
+internal static class UnitTestsHelper
+{
+
+    public static async Task<DogsAppDbContext> GetDatabaseContext()
+    {
+        var options = new DbContextOptionsBuilder<DogsAppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        var dbcontext = new DogsAppDbContext(options);
+        dbcontext.Database.EnsureCreated();
+        if (await dbcontext.Dogs.CountAsync() <= 0)
+        {
+            SeedData(dbcontext);
+        }
+
+        return dbcontext;
+    }
+
+    private async static void SeedData(DogsAppDbContext context)
+    {
+        await context.Dogs.AddRangeAsync(new Dog { Name = "Neo", Color = "red&amber", TailLength = 22, Weight = 32 },
+             new Dog { Name = "Jessy", Color = "black&yellow", TailLength = 7, Weight = 14 },
+             new Dog { Name = "Doggy", Color = "red", TailLength = 173, Weight = 33 }
+             );
+        await context.SaveChangesAsync();
+    }
+
+}
